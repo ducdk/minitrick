8ddf3a6c55ac46dcf8f16e25fd7ae0707e094e0e
ducdk <ducvuk6196@gmail.com>
2019-04-29 21:20:41 +0700

    update

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +7,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    this.menuTrigger();
+  }
+
   constructor() { }
 
   ngOnInit() {
+    this.menuTrigger();
+    this.themeToggle();
   }
 
+  themeToggle() {
+    const getTheme = localStorage && localStorage.getItem('minitrick-theme');
+    const isDark = getTheme === "dark";
+
+    if (getTheme !== null) {
+      document.body.classList.toggle("dark-theme", isDark);
+    }
+  }
+
+  changeTheme() {
+    document.body.classList.toggle("dark-theme");
+    localStorage.setItem(
+      'minitrick-theme',
+      document.body.classList.contains("dark-theme") ? "dark" : "light"
+    );
+  }
+
+  menuTrigger() {
+    const menuTrigger = document.querySelector(".menu-trigger");
+    const menu = document.querySelector(".menu");
+    const mobileQuery = getComputedStyle(document.body).getPropertyValue("--phoneWidth");
+    const isMobile = () => window.matchMedia(mobileQuery).matches;
+
+    menuTrigger && menuTrigger.classList.toggle("hidden", !isMobile());
+    menu && menu.classList.toggle("hidden", isMobile());
+  }
+
+  menuTriggerMobileShow() {
+    const menu = document.querySelector(".menu");
+    menu && menu.classList.toggle("hidden")
+  }
 }
